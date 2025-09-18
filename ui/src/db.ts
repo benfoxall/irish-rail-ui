ba3045d6ad6a8595a583c354c9370c14eebe13b6
Ben Foxall <benfoxall@gmail.com>
2025-09-18 18:09:21 +0100

    db access

---
+++ ui/src/db.ts
@@ -0,0 +1,57 @@
+import * as duckdb from "@duckdb/duckdb-wasm";
+import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url";
+import mvp_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url";
+import duckdb_wasm_eh from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url";
+import eh_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url";
+
+import dbUrl from "./assets/data.ddb?url";
+
+console.log(dbUrl);
+
+const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+  mvp: {
+    mainModule: duckdb_wasm,
+    mainWorker: mvp_worker,
+  },
+  eh: {
+    mainModule: duckdb_wasm_eh,
+    mainWorker: eh_worker,
+  },
+};
+// Select a bundle based on browser checks
+const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+// Instantiate the asynchronus version of DuckDB-wasm
+const worker = new Worker(bundle.mainWorker!);
+const logger = new duckdb.ConsoleLogger();
+export const db = new duckdb.AsyncDuckDB(logger, worker);
+
+await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+
+await db.registerFileURL(
+  "remote.duckdb",
+  dbUrl,
+  duckdb.DuckDBDataProtocol.HTTP,
+  false
+);
+
+await db.open({
+  path: "remote.duckdb",
+});
+
+console.log("open?");
+
+// Create a new connection
+const conn = await db.connect();
+
+const result = await conn.query(`
+    SELECT count(*) FROM stations
+`);
+
+console.log(
+  "result",
+  result.toArray().map((row) => row.toJSON())
+);
+
+window.sss = result;
+
+await conn.close();
